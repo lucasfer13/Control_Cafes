e33e93044b4c26c6ea00e9771eec889966bdcc67
formacio1 <j.avila@insjoanbrudieu.cat>
2023-05-23 13:30:43 +0200

    Navegación entre la pantalla login y signin funcional

--- app/src/main/java/com/example/cafes/screens/LoginApp.kt
+++ app/src/main/java/com/example/cafes/screens/LoginApp.kt
@@ -1,59 +1,40 @@
 package com.example.cafes.screens
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.cafes.R
-import com.example.cafes.ui.theme.CafesTheme
-
-class Registro : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            CafesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    SignInScreenLogo()
-                }
-            }
-        }
-    }
-}
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SignInScreen(){
+fun Body(
+    navController: NavController
+){
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -61,10 +42,8 @@ fun SignInScreen(){
     ) {
         val username = remember { mutableStateOf("") }
         val password = remember { mutableStateOf("") }
-        val confirmPassword = remember { mutableStateOf("") }
         val isUsernameError = remember { mutableStateOf(false) }
         val isPasswordError = remember { mutableStateOf(false) }
-        val isConfirmPasswordError = remember { mutableStateOf(false) }
 
         TextField(
             value = username.value,
@@ -98,67 +77,88 @@ fun SignInScreen(){
                 ),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword)
         )
-        TextField(
-            value = confirmPassword.value,
-            onValueChange = { value ->
-                confirmPassword.value = value
-                isConfirmPasswordError.value = !value.matches(Regex("[0123456789]"))
-                isConfirmPasswordError.value = value.length != 4
-                isConfirmPasswordError.value = value != password.value
-            },
-            label = { Text("Confirmar contraseña") },
-            modifier = Modifier
-                .padding(16.dp)
-                .border(
-                    width = 1.dp,
-                    color = if (isConfirmPasswordError.value) Color.Red else Color.Transparent,
-                    shape = RoundedCornerShape(4.dp)
-                ),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword)
-        )
-        Button(
-            onClick = {
-                      if (!isUsernameError.value && !isPasswordError.value && !isConfirmPasswordError.value) onSignInClick(username.value, password.value)
-            },
-            modifier = Modifier.padding(16.dp)
+
+        Row(
+            modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.Center
         ) {
-            Text("Dar de alta")
+            Button(
+                onClick = {
+                    if (!isUsernameError.value && !isPasswordError.value) onLogInClick(username.value, password.value)
+                },
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text("Entrar")
+            }
+            Button(
+                onClick = {
+                          navController.navigate(Screen.NewAccount.route)
+                },
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text("Crear usuario")
+            }
         }
     }
 }
 
-fun onSignInClick(username: String, password: String) {
-
+sealed class Screen(val route:String){
+    object LoginScreen: Screen(route = "loginscreen")
+    object NewAccount: Screen(route = "signinscreenlogo")
 }
 
 @Composable
-fun SignInScreenLogo(){
+fun CreateAccountScreen(){
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.LoginScreen.route
+    ) {
+        composable(route = Screen.LoginScreen.route){
+            LogInScreen(navController = navController)
+        }
+        composable(route = Screen.NewAccount.route) {
+            SignInScreenLogo()
+        }
+    }
+}
+
+@Composable
+fun LogInScreen(navController: NavController){
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        /*Image(
-            painter = painterResource(R.drawable.coffeeicon),
-            contentDescription = "Cafés express ícono",
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .padding(top = 16.dp)
-        )*/
-        Text(
-            text = "Made by Becarios",
-            style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 24.sp),
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .padding(top = 16.dp)
-        )
-        SignInScreen()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Row(
+                modifier = Modifier.padding(16.dp),
+                verticalAlignment = Alignment.Top
+            ) {
+                Text(
+                    text = "Cafetería Express",
+                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 24.sp),
+                    modifier = Modifier.padding(1.dp)
+                )
+            }
+            Row(
+                modifier = Modifier.padding(1.dp),
+                verticalAlignment = Alignment.Top
+            ) {
+                Text(
+                    text = "Made by Becarios",
+                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 12.sp),
+                    modifier = Modifier.padding(1.dp)
+                )
+            }
+        }
+        Body(navController = navController)
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview4() {
-    CafesTheme {
-        SignInScreenLogo()
-    }
+fun onLogInClick(username:String, password:String){
+
 }
