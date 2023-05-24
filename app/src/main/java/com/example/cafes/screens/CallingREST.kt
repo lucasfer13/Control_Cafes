635337eda69ff86cf9383edbb8acf95c454d7a28
formacio1 <j.avila@insjoanbrudieu.cat>
2023-05-24 10:59:26 +0200

    Cambiado REST para que nos dé la información de los cartones y no de los usuarios

--- app/src/main/java/com/example/cafes/screens/CallingREST.kt
+++ app/src/main/java/com/example/cafes/screens/CallingREST.kt
@@ -1,9 +1,12 @@
 package com.example.cafes.screens
 
+import com.example.cafes.models.Carton
 import com.example.cafes.models.User
+import com.example.cafes.repositorios.CartonRepository
 import com.example.cafes.repositorios.UserRepository
 
 val userRepository: UserRepository = UserRepository()
+val cartonRepository: CartonRepository = CartonRepository()
 
 fun call(){
     userRepository.init()
@@ -11,4 +14,8 @@ fun call(){
 
 fun getUsers(): ArrayList<User> {
     return userRepository.userlist
+}
+
+fun getCartons(): ArrayList<Carton> {
+    return cartonRepository.cartons
 }
