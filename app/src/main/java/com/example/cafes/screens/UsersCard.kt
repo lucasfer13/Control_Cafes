0cdcc7dcc9b5d4a6178fac460e438a6ca449a190
formacio1 <j.avila@insjoanbrudieu.cat>
2023-05-24 10:01:34 +0200

    Tarjetas de usuarios creadas

--- app/src/main/java/com/example/cafes/screens/UsersCard.kt
+++ app/src/main/java/com/example/cafes/screens/UsersCard.kt
@@ -1,13 +1,10 @@
 package com.example.cafes.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -15,9 +12,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -28,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -36,6 +35,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.cafes.R
 import com.example.cafes.models.Carton
 import com.example.cafes.models.User
 import com.example.cafes.ui.theme.CafesTheme
@@ -117,25 +117,56 @@ fun Body(
 }
 
 @Composable
-fun Cards(name: String, pack:String){
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = 8.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+fun Cards(nombres: ArrayList<String>, packs: ArrayList<String>){
+    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally) {
+        var i: Int = 0
+        while(i < nombres.size) {
+            if (i%2==0) {
+                Row{
+                    Card(nombres.get(i), packs.get(i))
+                    i++
+                    if (i < nombres.size) {
+                        Card(nombres.get(i), packs.get(i))
+                        i++
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Card(name: String, pack: String){
+    Card( Modifier.padding(10.dp)
+        //elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text(
-                text = name,
-                style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold),
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-            Text(
-                text = "Pack: ${pack} €",
-                style = TextStyle(fontSize = 14.sp)
-            )
+        Row {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = name,
+                    style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold),
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Text(
+                    text = "Pack: ${pack} €",
+                    style = TextStyle(fontSize = 14.sp)
+                )
+            }
+            Column (
+                modifier = Modifier.padding(16.dp)
+            ) {
+                IconButton(onClick = { /*TODO*/ },
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.coffee_icon),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .height(50.dp)
+                            .width(50.dp))
+                }
+            }
         }
     }
 }
@@ -149,7 +180,15 @@ fun Preview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Cards("Juan", "5")
+            val nombres: ArrayList<String> = ArrayList<String>()
+            nombres.add("Juan")
+            nombres.add("Lucas")
+            nombres.add("Montse")
+            val packs: ArrayList<String> = ArrayList<String>()
+            packs.add("5")
+            packs.add("10")
+            packs.add("20")
+            Cards(nombres, packs)
         }
     }
 }
