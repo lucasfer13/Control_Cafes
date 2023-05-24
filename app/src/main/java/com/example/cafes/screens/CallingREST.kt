9a7d8dc0cb3b568a906101d46378a04f40518c15
DiavoloCalvo <j.avila@insjoanbrudieu.cat>
2023-05-24 18:53:39 +0200

    Al hacer click en un usuario se pasa a la siguiente pantalla (elegir pack de cafés). Al seleccionarlo falta que se efectúe el nuevo cartón. Está casi a punto de terminarse

--- app/src/main/java/com/example/cafes/screens/CallingREST.kt
+++ app/src/main/java/com/example/cafes/screens/CallingREST.kt
@@ -3,14 +3,17 @@ package com.example.cafes.screens
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.MutableLiveData
 import com.example.cafes.models.Carton
+import com.example.cafes.models.TypeCarton
 import com.example.cafes.models.User
 import com.example.cafes.repositorios.CartonRepository
 import com.example.cafes.repositorios.UserRepository
 import com.example.cafes.viewModel.CartonViewModel
+import com.example.cafes.viewModel.TypeCartonViewModel
 import com.example.cafes.viewModel.UserViewModel
 
 val cartonViewModel: CartonViewModel = CartonViewModel()
 val userViewModel: UserViewModel = UserViewModel()
+val typeCartonViewModel: TypeCartonViewModel = TypeCartonViewModel()
 
 /*fun getUsers(): MutableLiveData<ArrayList<User>> {
     return userRepository.userlist
@@ -22,4 +25,8 @@ fun getCartons(): SnapshotStateList<Carton> {
 
 fun getUsers(): SnapshotStateList<User> {
     return userViewModel.getUsers()
+}
+
+fun getPacks(): SnapshotStateList<TypeCarton> {
+    return typeCartonViewModel.getTypeCartons()
 }
